0117108aad17ec3049382e0acdb62039bef6defb
antaschumacher <63446997+antaschumacher@users.noreply.github.com>
2020-04-10 18:45:35 +0700

    update lagi

--- assets/click.js
+++ assets/click.js
@@ -224,6 +224,32 @@ function daerahCorona() {
       <li>Merasa sakit? <strong>Usahakan menghubungi dokter melalui telepon terlebih dahulu agar dokter dapat mempersiapkan diri</strong></li>
     </ol>
     `;
+  } else if (kecamatanLower === "moyudan") {
+    kotakSatu.innerHTML = `
+    <h3>1</h3>
+    <p>Positif</p>
+    `;
+
+    kotakDua.innerHTML = `
+    <h3>3</h3>
+    <p>Pasien Dalam Pengawasan(PDP)</p>
+    `;
+
+    kotakTiga.innerHTML = `
+    <h3>53</h3>
+    <p>Orang Dalam Pemantauan (ODP)</p>
+    `;
+
+    kotakEmpat.innerHTML = `
+    <h2>Status: <span id="siaga">SIAGA</span></h2>
+    <h3>Apa yang harus anda lakukan?</h3>
+    <ol>
+      <li>Sebisa mungkin <strong>tetap berada di rumah</strong></li>
+      <li><strong>Jaga jarak</strong></li>
+      <li><strong>Cuci tangan</strong> setelah menyentuh barang apapun</li>
+      <li>Merasa sakit? <strong>Usahakan menghubungi dokter melalui telepon terlebih dahulu agar dokter dapat mempersiapkan diri</strong></li>
+    </ol>
+    `;
   }
 
   document.getElementById("jadisatu").appendChild(kotakSatu);
